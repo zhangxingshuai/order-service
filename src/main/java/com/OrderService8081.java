2a0c94eb28bc96b6daca293c52dae12d96d3f3c9
zhangxingshuai <210968635@qq.com>
2021-01-02 16:47:50 +0800

    订单模块

---
+++ src/main/java/com/OrderService8081.java
@@ -0,0 +1,11 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderService8081 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderService8081.class, args);
+    }
+}
